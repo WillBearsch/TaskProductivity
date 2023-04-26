a8963da5786f389da9ff71f6b899856dcbb2200f
William_Bearsch <wbearsc1@students.towson.edu>
2023-04-26 11:59:03 -0400

    Functionality finished, need css working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import TaskList from './Components/TaskList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          COSC 412 Task Productivity Web App
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TaskList />
     </div>
   );
 }
